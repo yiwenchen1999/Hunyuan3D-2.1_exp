6d07dbfea2cb460904f42247c608b665654cb88d
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-11 13:01:49 -0700

    test generate a mesh

--- generate_test.py
+++ generate_test.py
@@ -1,13 +1,17 @@
 import sys
 sys.path.insert(0, './hy3dshape')
 sys.path.insert(0, './hy3dpaint')
-from textureGenPipeline import Hunyuan3DPaintPipeline
 from textureGenPipeline import Hunyuan3DPaintPipeline, Hunyuan3DPaintConfig
 from hy3dshape.pipelines import Hunyuan3DDiTFlowMatchingPipeline
 
 # let's generate a mesh first
 shape_pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained('tencent/Hunyuan3D-2.1')
 mesh_untextured = shape_pipeline(image='assets/demo.png')[0]
+mesh_untextured.export('test_untextured.glb')
 
 paint_pipeline = Hunyuan3DPaintPipeline(Hunyuan3DPaintConfig(max_num_view=6, resolution=512))
-mesh_textured = paint_pipeline(output_mesh_path='test.glb', image_path='assets/demo.png')
+mesh_textured = paint_pipeline(
+    mesh_path='test_untextured.glb',
+    output_mesh_path='test.glb',
+    image_path='assets/demo.png',
+)
